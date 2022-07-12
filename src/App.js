4e6295139861f1c2ea0c273e8050e34de33e0c09
rajeshmn47 <rajeshmn47@gmail.com>
2022-07-12 08:04:48 +0530

    task completed

--- src/App.js
+++ src/App.js
@@ -2,22 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import axios from 'axios'
 import { useEffect,useState } from 'react';
-import Signup from './signup.js'
-import Login from './login.js'
-import Home from './home.js'
-import Addstudent from './addstudent.js';
+import Signup from './components/signup.js'
+import Login from './components/login.js'
+import Home from './components/home.js'
+import Addstudent from './components/addstudent.js';
 import {BrowserRouter,Routes,Route,Link,useNavigate} from 'react-router-dom';
 
 function App() {
-  const [data,setData]=useState()
-  async function fetchdata(){
-    const data=await axios.get('http://localhost:8000')
-    console.log(data.data.data)
-    setData(data.data.data)
-    }
-  useEffect(()=>{
-fetchdata()
-  },[])
+
   return (
   <>
   <div>
